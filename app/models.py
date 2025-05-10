7c5a5f884ab4cc06039fa2469663a7582bd13d4f
lizbethp9104 <lizbethp9104@gmail.com>
2025-05-09 22:37:24 -0700

    Notifications

--- app/models.py
+++ app/models.py
@@ -151,3 +151,15 @@ class Vote(db.Model):
         item_type = "Post" if self.post_id else "Reply"
 
         return f'<Vote {self.vote_type} by User {self.user_id} for {item_type} {item_id}>'
+    
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    is_read = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('User', backref='notifications')
+
+    def __repr__(self):
+        return f"<Notification to User {self.user_id}: {self.message}>"
